803678721bfc27d79ce9749b1d1b9a7cc37e86d8
Nukeador <nukeador@gmail.com>
2014-10-28 19:11:43 +0100

    DB updates just once per day. Fix #3

--- dashboard/management/commands/fetch_data.py
+++ dashboard/management/commands/fetch_data.py
@@ -17,7 +17,7 @@ URL = '/api/v1/rep/?format=json&limit=0'
 FILE = PROJECT_PATH + '/reps.json'
 
 class Command(BaseCommand):
-    args = '<profiles events>'
+    args = '<init_mentors updte>'
     help = 'Fetch json data from Reps API'
 
     def handle(self, *args, **options):
@@ -80,94 +80,110 @@ class Command(BaseCommand):
             self.stdout.write('Mentors data fetched. Please run manage.py fetch_data update')
         
         if 'update' in args:
-            response = requests.get(BASE_URL + URL, verify=False)
-        
-            if not response.status_code == 200:
-                raise CommandError('Invalid Response')
-    
-            data = response.json()
             
-            # to local file
-            with open(FILE, "w") as outfile:
-                json.dump(data, outfile)
-            
-            # to database
-            reps = Rep.objects.filter(deleted=False).order_by('uri')
-                    
-            # Updating fields from data fetched
-            for d in data['objects']:
-                
-                # Get mentor id
-                try:
-                    mentor = Rep.objects.get(uri=d['profile']['mentor'])
-                except Rep.DoesNotExist:
-                    mentor = None
-                
-                # If datetime is None, set it to 1970 to track it
-                if d['profile']['last_report_date'] is None:
-                    last_report_date = datetime.date(1970, 1, 1)
+            # Update just once a day
+            try:
+                s = Stat.objects.order_by('-date')
+                if timezone.now() - s[0].date < datetime.timedelta(1):
+                    do_update = False
                 else:
-                    last_report_date =  datetime.datetime.strptime(d['profile']['last_report_date'], '%Y-%m-%d').date()
+                    do_update = True
+
+            except:
+                do_update = True
+            
+            if do_update:
+                response = requests.get(BASE_URL + URL, verify=False)
+            
+                if not response.status_code == 200:
+                    raise CommandError('Invalid Response')
+        
+                data = response.json()
+                
+                # to local file
+                with open(FILE, "w") as outfile:
+                    json.dump(data, outfile)
+                
+                # to database
+                reps = Rep.objects.filter(deleted=False).order_by('uri')
                         
-                try:
-                    # If it's already on the database, update data
-                    r = Rep.objects.get(uri=d['resource_uri'], deleted=False)
+                # Updating fields from data fetched
+                for d in data['objects']:
                     
-                    r.first_name = d['first_name']
-                    r.last_name = d['last_name']
-                    r.is_mentor = d['profile']['is_mentor']
-                    r.is_council = d['profile']['is_council']
-                    r.avatar_url = d['profile']['avatar_url']
-                    r.profile_url = d['profile']['profile_url']
-                    r.mentor = mentor
-                    r.country = d['profile']['country']
-                    r.city = d['profile']['city']
-                    r.last_report_date = last_report_date
-                    r.updated_date = timezone.now()
+                    # Get mentor id
+                    try:
+                        mentor = Rep.objects.get(uri=d['profile']['mentor'])
+                    except Rep.DoesNotExist:
+                        mentor = None
                     
-                    r.save()
-                except Rep.DoesNotExist:
-                    # If it's not on the database we create it
-                    r = Rep(
-                        uri = d['resource_uri'],
-                        first_name = d['first_name'],
-                        last_name = d['last_name'],
-                        is_mentor = d['profile']['is_mentor'],
-                        is_council = d['profile']['is_council'],
-                        avatar_url = d['profile']['avatar_url'],
-                        profile_url = d['profile']['profile_url'],
-                        mentor = mentor,
-                        country = d['profile']['country'],
-                        city = d['profile']['city'],
-                        last_report_date = last_report_date,
-                        updated_date = timezone.now(),
-                    )
-                    r.save()
-                    
-                # check if someone was deleted from portal
-                for rep in reps:
-                    if not any(rep.uri in d['resource_uri'] for d in data['objects']):
-                        rep.deleted = True
-                        rep.save()
-                        print '%s is not longer on the portal, marking as deleted' % rep.full_name
+                    # If datetime is None, set it to 1970 to track it
+                    if d['profile']['last_report_date'] is None:
+                        last_report_date = datetime.date(1970, 1, 1)
+                    else:
+                        last_report_date =  datetime.datetime.strptime(d['profile']['last_report_date'], '%Y-%m-%d').date()
+                            
+                    try:
+                        # If it's already on the database, update data
+                        r = Rep.objects.get(uri=d['resource_uri'], deleted=False)
+                        
+                        r.first_name = d['first_name']
+                        r.last_name = d['last_name']
+                        r.is_mentor = d['profile']['is_mentor']
+                        r.is_council = d['profile']['is_council']
+                        r.avatar_url = d['profile']['avatar_url']
+                        r.profile_url = d['profile']['profile_url']
+                        r.mentor = mentor
+                        r.country = d['profile']['country']
+                        r.city = d['profile']['city']
+                        r.last_report_date = last_report_date
+                        r.updated_date = timezone.now()
+                        
+                        r.save()
+                    except Rep.DoesNotExist:
+                        # If it's not on the database we create it
+                        r = Rep(
+                            uri = d['resource_uri'],
+                            first_name = d['first_name'],
+                            last_name = d['last_name'],
+                            is_mentor = d['profile']['is_mentor'],
+                            is_council = d['profile']['is_council'],
+                            avatar_url = d['profile']['avatar_url'],
+                            profile_url = d['profile']['profile_url'],
+                            mentor = mentor,
+                            country = d['profile']['country'],
+                            city = d['profile']['city'],
+                            last_report_date = last_report_date,
+                            updated_date = timezone.now(),
+                        )
+                        r.save()
+                        
+                    # check if someone was deleted from portal
+                    for rep in reps:
+                        if not any(rep.uri in d['resource_uri'] for d in data['objects']):
+                            rep.deleted = True
+                            rep.save()
+                            print '%s is not longer on the portal, marking as deleted' % rep.full_name
+                
+                self.stdout.write('Profiles data updated')
+                
+                # Generate Stats
             
-            self.stdout.write('Profiles data updated')
+                # Query again with updated data
+                reps = Rep.objects.filter(deleted=False).order_by('uri')
+                
+                s = Stat(
+                    date = timezone.now(),
+                    reps = reps.count(),
+                    active = [r.status for r in reps].count('Active'),
+                    casual = [r.status for r in reps].count('Casual'),
+                    inactive = [r.status for r in reps].count('Inactive'),
+                    orphans = reps.filter(mentor=None).count(),
+                    mentors = reps.filter(is_mentor=True).count(),
+                )
+                
+                s.save()
+                
+                self.stdout.write('Data stats updated')
             
-            # Generate Stats
-            
-            # Query again with updated data
-            reps = Rep.objects.filter(deleted=False).order_by('uri')
-            
-            s = Stat(
-                date = timezone.now(),
-                reps = reps.count(),
-                active = [r.status for r in reps].count('Active'),
-                casual = [r.status for r in reps].count('Casual'),
-                inactive = [r.status for r in reps].count('Inactive'),
-                orphans = reps.filter(mentor=None).count(),
-                mentors = reps.filter(is_mentor=True).count(),
-            )
-            
-            s.save()
-            
-            self.stdout.write('Data stats updated')
+            else:
+                self.stdout.write('No need to update yet. Last update: %s' % s[0].date)
